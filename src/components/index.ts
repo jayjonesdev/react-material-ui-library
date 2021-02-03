0a5d01e78ef8cce357ebd8ffd43c5759e7724e3e
jayjonesdev <jayjonesdevelopment@gmail.com>
2021-02-03 10:22:08 -0500

    Version bump, remove Page Header from package

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,8 @@
-import { NavItem, MenuButton, PageHeader, SimpleDialog, MenuButtonItem, MultiSelect, CustomAccordion, ButtonBar, Checkbox, NavDropdownMenu, NavDropdownMenuItem } from './basic';
+import { NavItem, MenuButton, SimpleDialog, MenuButtonItem, MultiSelect, CustomAccordion, ButtonBar, Checkbox, NavDropdownMenu, NavDropdownMenuItem } from './basic';
 import { FlyIn, Appbar, Drawer } from './complex';
 
 export {
     MenuButton,
-    PageHeader,
     SimpleDialog,
     FlyIn,
     MenuButtonItem,
