18944102736a744a987554b8e87505e49bd51ba5
jayjonesdev <jayjonesdevelopment@gmail.com>
2021-05-30 08:43:50 -0400

    Loading Overaly background opacity change, open logic fixed

--- src/components/basic/loadingOverlay/index.tsx
+++ src/components/basic/loadingOverlay/index.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import { Box, Typography, CircularProgress, LinearProgress, Backdrop } from '@material-ui/core';
+import { Typography, CircularProgress, LinearProgress, Backdrop } from '@material-ui/core';
 import useStyles from './style';
 
 export default (props: IProps) => {
     const { label, color, type, open } = props;
-    const classes = useStyles();
+    const classes = useStyles({ type });
 
     return (
-        <Backdrop className={classes.root} open={open} onClick={() => { }}>
-
-            <Box
-                top={0}
-                left={0}
-                bottom={0}
-                right={0}
-                position="absolute"
-                display="flex"
-                flexDirection={type === 'circular' ? 'row' : 'column-reverse'}
-                alignItems="center"
-                justifyContent="center"
-            >
+        <Backdrop className={classes['$.MuiBackdrop-root']} open={open} invisible={open} onClick={() => { }}>
+            <div className={classes.content}>
                 {type === 'circular' ? <CircularProgress color={color} thickness={5} className={classes.circular} /> :
                     <LinearProgress color={color} className={classes.linear} />}
                 <Typography variant='h5' color={color}>{label}</Typography>
-            </Box>
+            </div>
         </Backdrop>
     )
 }
--- src/components/basic/loadingOverlay/style.tsx
+++ src/components/basic/loadingOverlay/style.tsx
@@ -1,10 +1,21 @@
 import { makeStyles } from '@material-ui/core';
 
 export default makeStyles(theme => ({
-    root: {
+    '$.MuiBackdrop-root': {
         zIndex: theme.zIndex.drawer + 1,
-        color: theme.palette.common.black,
+        backgroundColor: 'rgba(255, 255, 255, 0.45);',
+    },
+    content: {
+        display: 'flex',
+        flexDirection: (props: IStyleProps) => props.type === 'circular' ? 'row' : 'column-reverse',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%'
     },
     circular: { marginRight: theme.spacing(2) },
     linear: { height: 10, width: '25%', marginTop: theme.spacing(2) }
 }));
+
+interface IStyleProps {
+    type: 'circular' | 'linear';
+}
